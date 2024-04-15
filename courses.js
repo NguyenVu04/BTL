800e66377bd9ee50b3b83c21d62c0376cf3a0518
dinhduc0794 <duc.nguyendinh@hcmut.edu.vn>
2024-04-15 15:02:33 +0700

    dinhduc0794--fe-courses--add-dangkimon

--- courses.js
+++ courses.js
@@ -20,3 +20,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+
+const registBtn = document.querySelector('.regist-course')
+const modal = document.querySelector('.js-modal')
+const modalContainer = document.querySelector('.js-modal-container')
+const modalClose = document.querySelector('.js-modal-close')
+
+//Ham hien thi modal mua ve (them class open vao modal)
+function showCourseRegister() {
+    modal.classList.add('open')
+}
+
+registBtn.addEventListener('click', showCourseRegister)
+
+//Ham an modal mua ve (go bo class open cua modal)
+function hideCourseRegister() {
+    modal.classList.remove('open')
+}
+
+//nghe hanh vi click vao button close cua user, nghe dc thi goi ham an
+modalClose.addEventListener('click', hideCourseRegister)
+
+//bam vao ben ngoai cung tat giao dien modal 
+modal.addEventListener('click', hideCourseRegister)
+
+// khi bam vao form modal thi van se close do modal-container la con cua modal (tinh chat noi bot)
+//-> ngan chan t/c noi bot tu modal-container ra ben ngoai
+//event la bien tra ve
+modalContainer.addEventListener('click', function (event) {
+    event.stopPropagation() //ngung noi bot tu modalcontainer qua bien tra ve event, su kien click van dien ra nhung ko noi bot ra ben ngoai
+})  
+
+
+
+
+
